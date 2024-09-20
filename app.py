9e943e9016597ebf5c5bd7354904b8df745df607
Nikhil Pujari <mpnikhil@gmail.com>
2024-09-20 12:51:56 -0700

    Update CORS config

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# Allow only the frontend URL for CORS
+CORS(app, resources={r"/*": {"origins": "https://sacramento-mello-roos.vercel.app"}})
 
 @app.route('/get-tax-details', methods=['GET'])
 def get_tax_details():
